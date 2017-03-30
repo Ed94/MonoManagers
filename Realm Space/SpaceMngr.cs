4d6c185b5c3f56b4a40f955a58082af5a0bb06bd
Ed94 <edwardgz@gmail.com>
2017-03-30 18:06:26 -0400

    Add files via upload

---
+++ Realm Space/SpaceMngr.cs
@@ -0,0 +1,97 @@
+﻿//This is a massive WIP. Learning 3D implamentation for this game.
+//Monogame
+using Microsoft.Xna.Framework;
+//DIV
+using Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Dreams_in_Vapor;
+
+namespace AbstractRealm.Realm_Space
+{
+    public class SpaceMngr
+    {
+
+        //Related to the Camera.
+        private float   farPlaneDist;
+        private float   fieldOfView  ;
+        private float   nearPlaneDist;
+        public static  float scale;
+        
+        
+
+        public static Vector2 center;
+        public static Vector2 scaleVec2D;
+
+        public static Vector3 camPosition;
+        public static Vector3 camTarget  ;
+        public static Vector3 scaleVec3D;
+
+        public static Matrix view         ;   //ViewMatrix       Equivalent
+        public static Matrix camPerception;   //ProjectionMatrix Equivalent
+        public static Matrix area         ;   //Considered the world matrix by monogame. 
+
+        
+        private float  aspectRatio;
+
+
+
+        public SpaceMngr()
+        {
+            Console.WriteLine("We are in space manaer");
+            fieldOfView   = MathHelper.ToRadians(90f);
+
+            nearPlaneDist =     1f;
+            farPlaneDist  =  1000f;
+
+            camTarget     = Vector3.Zero;
+            camPosition   = new Vector3(0f, 0f,  -100);   //3D position of the player's perspective view for monogame.
+
+            center = new Vector2(AssetMngr.gDevice.Viewport.Width / 2, AssetMngr.gDevice.Viewport.Height / 2);
+
+            aspectRatio = (float)AssetMngr.gDevice.Viewport.Width / (float)AssetMngr.gDevice.Viewport.Height;
+            
+            scaleVec2D = getScale();
+            scale      = scaleVec2D.X;
+            scaleVec3D = new Vector3(scaleVec2D.X, scaleVec2D.Y, 0);
+            
+
+            Console.WriteLine(aspectRatio);
+            camPerception = Matrix.CreatePerspectiveFieldOfView(fieldOfView, AssetMngr.gDevice.Viewport.AspectRatio, nearPlaneDist, farPlaneDist);
+            view          = Matrix.CreateLookAt                (camPosition, camTarget                                   , Vector3.Up                 );
+            area          = Matrix.CreateWorld                 (camTarget  , Vector3.Forward                             , Vector3.Up                 );
+        }
+
+        public Matrix getCamPerception()
+        {
+            return camPerception;
+        }
+
+        public Vector2 getScale()
+        {
+            //Percent Difference from 720p
+            double percentWidth  = (double)AssetMngr.gDevice.Viewport.Width  / 3840;        Console.WriteLine("Percent Width: " + percentWidth);
+            double percentHeight = (double)AssetMngr.gDevice.Viewport.Height / 2160;
+
+            Vector2 newScale = new Vector2((float)percentWidth, (float)(percentHeight));
+
+            return newScale;
+        }
+    }
+}
+
+//General Stuff here.
+//private static float   angleInRadians;
+//private static float   pitch;
+//private static float   roll;
+//private static float   yaw;
+//private static Vector3 axis;
+//private static Vector3 position;
+
+//private static Matrix translation = Matrix.CreateTranslation     (position            ); A way to create a world matrix according to: http://rbwhitaker.wikidot.com/monogame-basic-matrices
+//private static Matrix rotationX   = Matrix.CreateRotationX       (angleInRadians      );
+//private static Matrix rotationY   = Matrix.CreateRotationY       (angleInRadians      );
+//private static Matrix rotationZ   = Matrix.CreateRotationZ       (angleInRadians      );
+//private static Matrix axisAngle   = Matrix.CreateFromAxisAngle   (axis, angleInRadians);
+//private static Matrix YPR         = Matrix.CreateFromYawPitchRoll(yaw , pitch, roll   );
+//private static Matrix rotMov = rotationX * translation;
---
+++ Realm Space/Triangle.cs
@@ -0,0 +1,119 @@
+﻿//Monogame
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+//DIV
+using Content;
+using System;
+
+namespace AbstractRealm.Realm_Space
+{
+    class Triangle : RealmControl
+    {
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer          vertexBuffer    ;
+
+        public Triangle()
+        {
+            Console.WriteLine("hi");
+            //Create triangle
+            triangleVertices    = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(  0,  20, 0), Color.White  );
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-20, -20, 0), Color.Gray   );
+            triangleVertices[2] = new VertexPositionColor(new Vector3( 20, -20, 0), Color.DimGray);
+
+            vertexBuffer = new VertexBuffer(AssetMngr.gDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices);
+        }
+
+        public override void Update()
+        {
+            //Basic Hori and Vert
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                SpaceMngr.camPosition.X -= 1f;
+                SpaceMngr.camTarget  .X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                SpaceMngr.camPosition.X += 1f;
+                SpaceMngr.camTarget  .X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                SpaceMngr.camPosition.Y -= 1f;
+                SpaceMngr.camTarget  .Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                SpaceMngr.camPosition.Y += 1f;
+                SpaceMngr.camTarget  .Y += 1f;
+            }
+
+            //Rotation
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(1f));
+                SpaceMngr.camPosition = Vector3.Transform(SpaceMngr.camPosition, rotationMatrix);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(-1f));
+                SpaceMngr.camPosition = Vector3.Transform(SpaceMngr.camPosition, rotationMatrix);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationX(MathHelper.ToRadians(1f));
+                SpaceMngr.camPosition = Vector3.Transform(SpaceMngr.camPosition, rotationMatrix);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationX(MathHelper.ToRadians(-1f));
+                SpaceMngr.camPosition = Vector3.Transform(SpaceMngr.camPosition, rotationMatrix);
+            }
+
+            //Zoom
+            if (Keyboard.GetState().IsKeyDown(Keys.LeftShift))
+            {
+                SpaceMngr.camPosition.Z += 1f;
+                SpaceMngr.camTarget.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.LeftControl))
+            {
+                SpaceMngr.camPosition.Z -= 1f;
+                SpaceMngr.camTarget.Z -= 1f;
+            }
+
+            SpaceMngr.view = Matrix.CreateLookAt(SpaceMngr.camPosition, SpaceMngr.camTarget, Vector3.Up);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, BasicEffect basicEffect)
+        {
+            basicEffect.Alpha              = 1.0f ;
+            basicEffect.TextureEnabled     = false;
+            basicEffect.VertexColorEnabled = true ;
+            basicEffect.LightingEnabled    = false;
+
+            basicEffect.Projection = SpaceMngr.camPerception;
+            basicEffect.View       = SpaceMngr.view         ;
+            basicEffect.World      = SpaceMngr.area         ; 
+
+            AssetMngr.gDevice.SetVertexBuffer(vertexBuffer);
+
+            // Turn off backface culling
+            RasterizerState rasterizerState = new RasterizerState();
+
+            rasterizerState.CullMode = CullMode.None;
+            //rasterizerState.FillMode = FillMode.WireFrame;
+
+            AssetMngr.gDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                AssetMngr.gDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 3);
+            }
+        }
+    }
+}
