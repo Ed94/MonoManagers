cc9a4746b15b899d94619440b63390d6228af3ab
Ed94 <edwardgz@gmail.com>
2017-03-30 18:20:17 -0400

    Update TestBill.cs

--- States/Test/TestBill.cs
+++ States/Test/TestBill.cs
@@ -1,59 +1,21 @@
 ﻿//C#
-using System;
-//Monogame
-using Microsoft.Xna.Framework.Graphics;
-//DIV
-using AbstractRealm.Realm_Space;
-using Microsoft.Xna.Framework;
-using Dreams_in_Vapor;
-using Microsoft.Xna.Framework.Input;
-using Content;
+using System                    ;
 using System.Collections.Generic;
+//Monogame
+using Microsoft.Xna.Framework         ;
+using Microsoft.Xna.Framework.Input   ;
+using Microsoft.Xna.Framework.Graphics;
+//Game
+using Content                  ;
+using Game_Namespace           ;
+using AbstractRealm.Realm_Space;
+
 
 namespace AbstractRealm.States
 {
     class TestBill : RealmControl
     {
-        Vector3 playerPosition ;
-        Vector3 conceptPosition;
-
-        RigidBillboard player       ;
-        RigidBillboard concept_level;
-
-        Texture2D femaleIdle    ;
-        Texture2D conceptTexture;
-
-        public TestBill()
-        {
-            assetMngr.LoadStateResource("no_clothes");
-            assetMngr.LoadStateResource("slum"      );
-
-            foreach (KeyValuePair<string, Texture2D> texture in AssetMngr.currentTextures)
-            {
-                if (texture.Key.Contains("f_idleTexture"))
-                {
-
-                }
-                else if (texture.Key.Contains("f_idle"))
-                {
-                    femaleIdle = texture.Value;
-                }
-
-                if (texture.Key.Contains("concept"))
-                {
-                    conceptTexture = texture.Value;
-                }
-            }
-
-            Vector3 femaleSize = new Vector3(  52, 188, 0);
-            Vector3 bkSize     = new Vector3(1780, 977, 0);
-
-            playerPosition  = Vector3.Zero;
-            conceptPosition = bkSize / 8  ;
-
-            player        = new RigidBillboard(femaleIdle          , Vector3.Zero   , Vector3.Multiply(femaleSize, 0.25f));
-            concept_level = new RigidBillboard(conceptTexture   , conceptPosition,     Vector3.Multiply(bkSize, 0.25f));
-        }
+        public TestBill() {}
 
         public override void Update()
         {
@@ -65,11 +27,6 @@ namespace AbstractRealm.States
                 stateMngr.setCRTState(StateMngr.ARstate.TestTri);
             }
         }
-
-        public override void Draw(SpriteBatch spriteBatch, BasicEffect basicEffect)
-        {
-            concept_level.Draw(spriteBatch, basicEffect);
-            player       .Draw(spriteBatch, basicEffect);
-        }
+        public override void Draw(SpriteBatch spriteBatch, BasicEffect basicEffect) {}
     }
 }
